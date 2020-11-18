6e4b0892dabc8fd258a9fa9de6ed3978c6c15692
Siva Nadupuru <siva.nadupuru@gmail.com>
2020-11-18 13:29:54 +0530

    calldetails micro service

---
+++ calldetails-micro/src/main/java/com/calldetails/service/ICallDetailsService.java
@@ -0,0 +1,10 @@
+package com.calldetails.service;
+
+import java.util.List;
+
+import com.calldetails.dto.CallDetailsDto;
+
+public interface ICallDetailsService {
+	List<CallDetailsDto>  readCallDetails(Long  phoneNumber);
+
+}
