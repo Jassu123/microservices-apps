81a7f93ef87f4515b7f31e1a889837405b65c9c0
Siva Nadupuru <siva.nadupuru@gmail.com>
2020-11-18 13:35:28 +0530

    plans micro service

---
+++ plan-micro/src/main/java/com/plan/PlanMicroApplication.java
@@ -0,0 +1,16 @@
+package com.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+
+@EnableDiscoveryClient
+public class PlanMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanMicroApplication.class, args);
+	}
+
+}
